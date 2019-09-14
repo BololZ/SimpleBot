e58000d1cdb889c293db5884588460136fb3dfb3
Bolol <43433312+BololZ@users.noreply.github.com>
2019-09-14 19:23:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import logging
 import bot
+import yaml
+
 
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='log/discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
 
+with open("config.yml", 'r') as stream:
+    try:
+        cfg = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+token = cfg['client']['token']
+bot.chan_id = cfg['client']['chan_id']
+
 client = bot.MyClient()
-client.run('NjIwNDc0OTkyNjA5NzIyMzg3.XXXbhA.bcr9VOVoj11NujMumGcsK0lleW8')
+client.run(token)
