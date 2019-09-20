341531edb47c7b154ba30dae87c4b67cd40e5409
Bolol <43433312+BololZ@users.noreply.github.com>
2019-09-20 02:10:44 +0200

    Bot.py
    Add on new message

--- bot.py
+++ bot.py
@@ -12,17 +12,23 @@ class MyClient(discord.AutoShardedClient):
 
     async def on_ready(self):
         print('Logged on as', self.user)
-        print(chan_id)
+        print('Chan_id :', chan_id)
 
     async def backgroung_task_chut(self):
         await self.wait_until_ready()
-        channel = self.get_channel(int(chan_id))  # channel ID goes here Bolol
+        channel = self.get_channel(int(chan_id))
+        last_msg_id = channel.last_message_id
         while not self.is_closed():
             heure = datetime.datetime.now()
             h = str(heure.hour)
             m = str(heure.minute)
             print(h+':'+m)
-            if (int(h) >= 22 or int(h) <= 6) and (int(m) == 0 or int(m) == 30):
-                print('Chut.......<3')
-                await channel.send('Chut.....:heart:', tts=True)
+            next_msg_id = channel.last_message_id
+            print('Last ID Msg :', last_msg_id)
+            print('Next ID Msg :',next_msg_id)
+            if (int(h) >= 22 or int(h) <=5) and (int(m)%30 == 0):
+                if last_msg_id != next_msg_id:
+                    print('Chut.......<3')
+                    await channel.send('Chut.....:heart:')
+                    last_msg_id = channel.last_message_id
             await asyncio.sleep(60)
