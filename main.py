365a33aba3a04e0d3fab41fd960c76ab497740de
Bolol <43433312+BololZ@users.noreply.github.com>
2019-11-01 13:12:48 +0100

    Update main.py
    Add some cfg and error

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import logging
 import bot
 import yaml
 
-
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='log/discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-
 
 with open("config.yml", 'r') as stream:
     try:
@@ -16,7 +14,9 @@ with open("config.yml", 'r') as stream:
     except yaml.YAMLError as exc:
         print(exc)
 token = cfg['client']['token']
-bot.chan_id = cfg['client']['chan_id']
 
 client = bot.MyClient()
-client.run(token)
+try:
+    client.run(token)
+except:
+    print('Erreur d\'éxécution')
